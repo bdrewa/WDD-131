973463cd6cacc064be7af6dbf270692f36deb6d8
bdrewa <bre17014@byui.edu>
2025-12-10 22:34:02 -0700

    Finished cool pics 2

---
+++ coolpics_p2/script.js
@@ -0,0 +1,65 @@
+var menuButton = document.querySelector('.menu-button');
+var navLinks = document.getElementById('nav-links');
+
+menuButton.addEventListener('click', function() {
+  if (navLinks.classList.contains('hide')) {
+    navLinks.classList.remove('hide');
+    menuButton.setAttribute('aria-expanded', 'true');
+  } else {
+    navLinks.classList.add('hide');
+    menuButton.setAttribute('aria-expanded', 'false');
+  }
+});
+
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    navLinks.classList.remove('hide');
+  } else {
+    navLinks.classList.add('hide');
+    menuButton.setAttribute('aria-expanded', 'false');
+  }
+}
+
+window.addEventListener('resize', handleResize);
+handleResize();
+
+var gallery = document.querySelector('.gallery');
+
+function viewerTemplate(pic, words) {
+  return '<div class="viewer-content"><img src="' + pic + '" alt="' + words + '"><button class="close-viewer">X</button></div>';
+}
+
+function openViewer(img) {
+  var src = img.src;
+  var alt = img.alt;
+  
+  var parts = src.split('-');
+  var firstPart = parts[0];
+  var lastPart = parts[parts.length - 1];
+  var fullSrc = firstPart + '-full.' + lastPart.split('.')[1];
+
+  var dialog = document.createElement('dialog');
+  dialog.innerHTML = viewerTemplate(fullSrc, alt);
+  document.body.appendChild(dialog);
+  dialog.showModal();
+
+  var closeButton = dialog.querySelector('.close-viewer');
+  closeButton.addEventListener('click', function() {
+    dialog.close();
+    document.body.removeChild(dialog);
+  });
+
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close();
+      document.body.removeChild(dialog);
+    }
+  });
+}
+
+gallery.addEventListener('click', function(event) {
+  var clickedImg = event.target.closest('img');
+  if (clickedImg) {
+    openViewer(clickedImg);
+  }
+});
